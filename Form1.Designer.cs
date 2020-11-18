704f3fdbedd1fe7146f9be15fe128893d6d4f65a
Benjamin Tilinko <41190590+tilben97@users.noreply.github.com>
2020-11-18 09:27:37 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace filterc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Port_Box = new System.Windows.Forms.ComboBox();
+            this.BaudRate_Box = new System.Windows.Forms.ComboBox();
+            this.Acc_lbl = new System.Windows.Forms.Label();
+            this.Filter_lbl = new System.Windows.Forms.Label();
+            this.Acc_Avg = new System.Windows.Forms.Label();
+            this.Filter_Avg = new System.Windows.Forms.Label();
+            this.connect_btn = new System.Windows.Forms.Button();
+            this.disconnect_btn = new System.Windows.Forms.Button();
+            this.mpu_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pos_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pos_lbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mpu_chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos_chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Port_Box
+            // 
+            this.Port_Box.FormattingEnabled = true;
+            this.Port_Box.Location = new System.Drawing.Point(29, 32);
+            this.Port_Box.Name = "Port_Box";
+            this.Port_Box.Size = new System.Drawing.Size(121, 24);
+            this.Port_Box.TabIndex = 0;
+            // 
+            // BaudRate_Box
+            // 
+            this.BaudRate_Box.FormattingEnabled = true;
+            this.BaudRate_Box.Items.AddRange(new object[] {
+            "115200",
+            "9600",
+            "128000"});
+            this.BaudRate_Box.Location = new System.Drawing.Point(176, 32);
+            this.BaudRate_Box.Name = "BaudRate_Box";
+            this.BaudRate_Box.Size = new System.Drawing.Size(121, 24);
+            this.BaudRate_Box.TabIndex = 1;
+            // 
+            // Acc_lbl
+            // 
+            this.Acc_lbl.AutoSize = true;
+            this.Acc_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Acc_lbl.Location = new System.Drawing.Point(822, 48);
+            this.Acc_lbl.Name = "Acc_lbl";
+            this.Acc_lbl.Size = new System.Drawing.Size(77, 32);
+            this.Acc_lbl.TabIndex = 2;
+            this.Acc_lbl.Text = "Acc: ";
+            // 
+            // Filter_lbl
+            // 
+            this.Filter_lbl.AutoSize = true;
+            this.Filter_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Filter_lbl.Location = new System.Drawing.Point(822, 97);
+            this.Filter_lbl.Name = "Filter_lbl";
+            this.Filter_lbl.Size = new System.Drawing.Size(94, 32);
+            this.Filter_lbl.TabIndex = 3;
+            this.Filter_lbl.Text = "Filter: ";
+            // 
+            // Acc_Avg
+            // 
+            this.Acc_Avg.AutoSize = true;
+            this.Acc_Avg.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Acc_Avg.Location = new System.Drawing.Point(1417, 48);
+            this.Acc_Avg.Name = "Acc_Avg";
+            this.Acc_Avg.Size = new System.Drawing.Size(126, 32);
+            this.Acc_Avg.TabIndex = 4;
+            this.Acc_Avg.Text = "Acc Avg:";
+            // 
+            // Filter_Avg
+            // 
+            this.Filter_Avg.AutoSize = true;
+            this.Filter_Avg.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Filter_Avg.Location = new System.Drawing.Point(1417, 97);
+            this.Filter_Avg.Name = "Filter_Avg";
+            this.Filter_Avg.Size = new System.Drawing.Size(143, 32);
+            this.Filter_Avg.TabIndex = 5;
+            this.Filter_Avg.Text = "Filter Avg:";
+            this.Filter_Avg.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // connect_btn
+            // 
+            this.connect_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect_btn.Location = new System.Drawing.Point(303, 32);
+            this.connect_btn.Name = "connect_btn";
+            this.connect_btn.Size = new System.Drawing.Size(138, 31);
+            this.connect_btn.TabIndex = 6;
+            this.connect_btn.Text = "Connect";
+            this.connect_btn.UseVisualStyleBackColor = true;
+            this.connect_btn.Click += new System.EventHandler(this.connect_btn_Click);
+            // 
+            // disconnect_btn
+            // 
+            this.disconnect_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.disconnect_btn.Location = new System.Drawing.Point(447, 32);
+            this.disconnect_btn.Name = "disconnect_btn";
+            this.disconnect_btn.Size = new System.Drawing.Size(138, 31);
+            this.disconnect_btn.TabIndex = 7;
+            this.disconnect_btn.Text = "DiscConnect";
+            this.disconnect_btn.UseVisualStyleBackColor = true;
+            this.disconnect_btn.Click += new System.EventHandler(this.disconnect_btn_Click);
+            // 
+            // mpu_chart
+            // 
+            this.mpu_chart.BackColor = System.Drawing.SystemColors.Highlight;
+            chartArea3.AxisX.Maximum = 300D;
+            chartArea3.AxisX.Minimum = 0D;
+            chartArea3.AxisX2.Maximum = 300D;
+            chartArea3.AxisX2.Minimum = 0D;
+            chartArea3.AxisY.Maximum = 1D;
+            chartArea3.AxisY.Minimum = -1D;
+            chartArea3.AxisY2.Maximum = 1D;
+            chartArea3.AxisY2.Minimum = -1D;
+            chartArea3.Name = "ChartArea1";
+            this.mpu_chart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.mpu_chart.Legends.Add(legend3);
+            this.mpu_chart.Location = new System.Drawing.Point(12, 149);
+            this.mpu_chart.Name = "mpu_chart";
+            series6.BorderWidth = 3;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Color = System.Drawing.Color.Blue;
+            series6.Legend = "Legend1";
+            series6.Name = "Filter";
+            series7.BorderWidth = 3;
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Color = System.Drawing.Color.Gold;
+            series7.Legend = "Legend1";
+            series7.Name = "Acc";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Color = System.Drawing.Color.Lime;
+            series8.Legend = "Legend1";
+            series8.Name = "Reference";
+            this.mpu_chart.Series.Add(series6);
+            this.mpu_chart.Series.Add(series7);
+            this.mpu_chart.Series.Add(series8);
+            this.mpu_chart.Size = new System.Drawing.Size(1929, 465);
+            this.mpu_chart.TabIndex = 8;
+            this.mpu_chart.Text = "chart1";
+            // 
+            // start_btn
+            // 
+            this.start_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_btn.Location = new System.Drawing.Point(29, 87);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(157, 56);
+            this.start_btn.TabIndex = 9;
+            this.start_btn.Text = "START";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop_btn.Location = new System.Drawing.Point(215, 87);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(157, 56);
+            this.stop_btn.TabIndex = 10;
+            this.stop_btn.Text = "STOP";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pos_chart
+            // 
+            this.pos_chart.BackColor = System.Drawing.SystemColors.Highlight;
+            chartArea4.AxisX.Maximum = 300D;
+            chartArea4.AxisX.Minimum = 0D;
+            chartArea4.AxisX2.Maximum = 300D;
+            chartArea4.AxisX2.Minimum = 0D;
+            chartArea4.AxisY.Maximum = 1D;
+            chartArea4.AxisY.Minimum = -1D;
+            chartArea4.AxisY2.Maximum = 1D;
+            chartArea4.AxisY2.Minimum = -1D;
+            chartArea4.Name = "ChartArea1";
+            this.pos_chart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.pos_chart.Legends.Add(legend4);
+            this.pos_chart.Location = new System.Drawing.Point(12, 620);
+            this.pos_chart.Name = "pos_chart";
+            series9.BorderWidth = 3;
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series9.Color = System.Drawing.Color.DeepPink;
+            series9.Legend = "Legend1";
+            series9.Name = "Position";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series10.Color = System.Drawing.Color.Lime;
+            series10.Legend = "Legend1";
+            series10.Name = "Reference";
+            this.pos_chart.Series.Add(series9);
+            this.pos_chart.Series.Add(series10);
+            this.pos_chart.Size = new System.Drawing.Size(1929, 462);
+            this.pos_chart.TabIndex = 11;
+            this.pos_chart.Text = "chart1";
+            // 
+            // pos_lbl
+            // 
+            this.pos_lbl.AutoSize = true;
+            this.pos_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pos_lbl.Location = new System.Drawing.Point(402, 97);
+            this.pos_lbl.Name = "pos_lbl";
+            this.pos_lbl.Size = new System.Drawing.Size(126, 32);
+            this.pos_lbl.TabIndex = 12;
+            this.pos_lbl.Text = "Position:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1972, 1119);
+            this.Controls.Add(this.pos_lbl);
+            this.Controls.Add(this.pos_chart);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.mpu_chart);
+            this.Controls.Add(this.disconnect_btn);
+            this.Controls.Add(this.connect_btn);
+            this.Controls.Add(this.Filter_Avg);
+            this.Controls.Add(this.Acc_Avg);
+            this.Controls.Add(this.Filter_lbl);
+            this.Controls.Add(this.Acc_lbl);
+            this.Controls.Add(this.BaudRate_Box);
+            this.Controls.Add(this.Port_Box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mpu_chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos_chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Port_Box;
+        private System.Windows.Forms.ComboBox BaudRate_Box;
+        private System.Windows.Forms.Label Acc_lbl;
+        private System.Windows.Forms.Label Filter_lbl;
+        private System.Windows.Forms.Label Acc_Avg;
+        private System.Windows.Forms.Label Filter_Avg;
+        private System.Windows.Forms.Button connect_btn;
+        private System.Windows.Forms.Button disconnect_btn;
+        private System.Windows.Forms.DataVisualization.Charting.Chart mpu_chart;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart pos_chart;
+        private System.Windows.Forms.Label pos_lbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace filterc
+{
+    public partial class Form1 : Form
+    {
+        int thread_value = 93;
+        double range3 = 0, range4 = 0;
+        double range_pos_min = 0, range_pos_max = 0;
+        string[] ports;
+        int n = 0;
+        int m = 0;
+        double reference = 0;
+        bool recieve = false;
+        static int uart_size = 100;
+        static int Rx_size = 36;
+        byte[] data_out = new byte[Rx_size];
+        byte[] uart_buffer = new byte[uart_size];
+        char header1;
+        char header2;
+        char header3;
+        char header4;
+        char footer1;
+        char footer2;
+        char footer3;
+        char footer4;
+        double roll_average = 0, Sum = 0;
+        double filter_average = 0, Sum2 = 0;
+        int cnt_average = 0;
+        double[] pos = new double[10];
+        double[] filter = new double[10];
+        double[] rollAcc = new double[10];
+
+        private void disconnect_btn_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            disconnect_btn.Enabled = false;
+            connect_btn.Enabled = true;
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            recieve = true;
+            start_btn.Enabled = false;
+            stop_btn.Enabled = true;
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            recieve = false;
+            start_btn.Enabled = true;
+            stop_btn.Enabled = false;
+            timer1.Stop();
+            this.Invoke((MethodInvoker)delegate { delete_charts(); });
+        }
+
+        private void delete_charts()
+        {
+            pos_chart.Series["Position"].Points.Clear();
+            pos_chart.Series["Reference"].Points.Clear();
+            mpu_chart.Series["Acc"].Points.Clear();
+            mpu_chart.Series["Filter"].Points.Clear();
+            mpu_chart.Series["Reference"].Points.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (recieve)
+            {
+                n = 0;
+                serialPort1.Read(uart_buffer, 0, uart_size);
+                while (n <= uart_size - 36)
+                {
+                    header1 = (char)uart_buffer[n];
+                    header2 = (char)uart_buffer[n + 1];
+                    header3 = (char)uart_buffer[n + 2];
+                    header4 = (char)uart_buffer[n + 3];
+                    if (header1 == 'G' && header2 == 'o' && header3 == 'g' && header4 == 'o')
+                    {
+                        footer1 = (char)uart_buffer[n + 32];
+                        footer2 = (char)uart_buffer[n + 33];
+                        footer3 = (char)uart_buffer[n + 34];
+                        footer4 = (char)uart_buffer[n + 35];
+                        if (footer1 == 'B' && footer2 == 'e' && footer3 == 'b' && footer4 == 'e')
+                        {
+                            filter[m] = BitConverter.ToDouble(uart_buffer, n + 8);
+                            rollAcc[m] = BitConverter.ToDouble(uart_buffer, n + 16);
+                            pos[m] = BitConverter.ToDouble(uart_buffer, n + 24);
+                            n = n + 35;
+                            Sum = Sum + rollAcc[m];
+                            Sum2 = Sum2 + filter[m];
+                            m++;
+                            cnt_average++;
+                            if (cnt_average > 99)
+                            {
+                                roll_average = Sum / 100;
+                                filter_average = Sum2 / 100;
+                                Acc_Avg.Text = "Acc Avg: " + roll_average.ToString();
+                                Filter_Avg.Text = "Filter Avg: " + filter_average.ToString();
+                                Sum = 0;
+                                Sum2 = 0;
+                                cnt_average = 0;
+                            }
+                            if (m > 9)
+                            {
+                                m = 0;
+                                Thread.Sleep(thread_value);
+                                for (int q = 0; q < 9; q++)
+                                {
+                                    if (range3 < rollAcc[q])
+                                    {
+                                        range3 = rollAcc[q];
+                                    }
+                                    else if (range3 < filter[q])
+                                    {
+                                        range3 = filter[q];
+                                    }
+                                    if (range4 > rollAcc[q])
+                                    {
+                                        range4 = rollAcc[q];
+                                    }
+                                    else if (range4 > filter[q])
+                                    {
+                                        range4 = filter[q];
+                                    }
+                                    if (range_pos_min > pos[q])
+                                    {
+                                        range_pos_min = pos[q];
+                                    }
+                                    if (range_pos_max < pos[q])
+                                    {
+                                        range_pos_max = pos[q];
+                                    }
+                                    mpu_chart.ChartAreas[0].AxisY.Maximum = range3;
+                                    mpu_chart.ChartAreas[0].AxisY.Minimum = range4;
+                                    pos_chart.ChartAreas[0].AxisY.Minimum = range_pos_min;
+                                    pos_chart.ChartAreas[0].AxisY.Maximum = range_pos_max;
+                                    pos_chart.Series["Position"].Points.AddY(pos[q]);
+                                    pos_chart.Series["Reference"].Points.AddY(reference);
+                                    mpu_chart.Series["Acc"].Points.AddY(rollAcc[q]);
+                                    mpu_chart.Series["Filter"].Points.AddY(filter[q]);
+                                    mpu_chart.Series["Reference"].Points.AddY(reference);
+                                    if (mpu_chart.Series[0].Points.Count == 300)
+                                    {
+                                        mpu_chart.Series[0].Points.RemoveAt(0);
+                                    }
+                                    if (mpu_chart.Series[1].Points.Count == 300)
+                                    {
+                                        mpu_chart.Series[1].Points.RemoveAt(0);
+                                    }
+                                    if (mpu_chart.Series[2].Points.Count == 300)
+                                    {
+                                        mpu_chart.Series[2].Points.RemoveAt(0);
+                                    }
+                                    if (pos_chart.Series[0].Points.Count == 300)
+                                    {
+                                        pos_chart.Series[0].Points.RemoveAt(0);
+                                    }
+                                    if (pos_chart.Series[1].Points.Count == 300)
+                                    {
+                                        pos_chart.Series[1].Points.RemoveAt(0);
+                                    }
+                                }
+                                pos_lbl.Text = "Position: " + pos[5].ToString();
+                                Acc_lbl.Text = "Acc: " + rollAcc[5].ToString();
+                                Filter_lbl.Text = "Filter: " + filter[5].ToString();
+
+                            }
+
+                        }
+                        else
+                        {
+                            n++;
+                        }
+                    }
+                    else
+                    {
+                        n++;
+                    }
+                }
+
+            }
+            else
+            {
+                //
+            }
+
+
+        }
+
+        private void connect_btn_Click(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = Convert.ToInt32(BaudRate_Box.Text);
+            serialPort1.PortName = Port_Box.Text;
+            serialPort1.ReadBufferSize = uart_size;
+            serialPort1.Open();
+            if (Convert.ToInt32(BaudRate_Box.Text) == 115200)
+            {
+                thread_value = 97;
+                timer1.Interval = 3;
+            }
+            else if (Convert.ToInt32(BaudRate_Box.Text) == 9600)
+            {
+                thread_value = 900;
+                timer1.Interval = 10;
+            }
+            else if (Convert.ToInt32(BaudRate_Box.Text) == 128000)
+            {
+                thread_value = 29;
+                timer1.Interval = 1;
+            }
+            connect_btn.Enabled = false;
+            disconnect_btn.Enabled = true;
+            n = 0;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ports = SerialPort.GetPortNames();
+            Port_Box.Items.AddRange(ports);
+            disconnect_btn.Enabled = false;
+            start_btn.Enabled = true;
+            stop_btn.Enabled = false;
+        }
+    }
+}
